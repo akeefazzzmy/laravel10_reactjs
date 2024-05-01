1413ff6178cf0c7d1524dafee2d097ba522e9e2f
akeefazzzmy <m.akif.azmi@gmail.com>
2024-05-01 23:43:31 +0800

    Add Todo Seeder

---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Todo;
+use Database\Factories\TodoFactory;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Use the factory to create fake todos
+        Todo::factory()->count(10)->create();
+    }
+}
